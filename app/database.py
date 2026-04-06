41230a2c30df65aec3a82d96b531d840476d5f0c
Doctor Strange <strange@avengers.dev>
2026-04-06 00:43:26 -0400

    hotfix: switch to in-memory SQLite to bypass Render filesystem restrictions

--- app/database.py
+++ app/database.py
@@ -3,7 +3,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 import os
 
-SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:////tmp/hosteva.db")
+SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL", "sqlite://")
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False} if SQLALCHEMY_DATABASE_URL.startswith("sqlite") else {}
